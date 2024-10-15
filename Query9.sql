43897414cfd766355067dbf9ed8a39bd755e2560
Diya Wadhwani <wadhwani2399@gmail.com>
2024-10-14 19:11:12 -0700

    Updated Query9

--- Query9.sql
+++ Query9.sql
@@ -1,8 +1,11 @@
--- Query 9 - Creative Addition: Min, Max and Avg File Sizes in MB of each Genre
+-- Query 9 - Creative Addition: Min, Max and Avg File Sizes in MB of each Genre and No. of Albums from each Genre
 
 SELECT g.GenreId, g.Name AS GenreName, 
 	MIN(ROUND(t.bytes / 1024.0 / 1024.0, 2)) AS MinFileSize_MB, 
 	ROUND(AVG(t.bytes / 1024.0 / 1024.0),2) AS AvgFileSize_MB, 
-	MAX(ROUND(t.bytes / 1024.0 / 1024.0, 2)) AS MaxFileSize_MB FROM genres g
+	MAX(ROUND(t.bytes / 1024.0 / 1024.0, 2)) AS MaxFileSize_MB,
+	COUNT( DISTINCT al.AlbumId) AS AlbumCount
+	FROM genres g
 	INNER JOIN tracks t ON t.GenreId = g.GenreId
+	INNER JOIN albums al ON al.AlbumId = t.AlbumId
 	GROUP BY g.GenreId;
